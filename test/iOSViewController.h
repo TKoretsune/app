a7b67224964d1bfc911dff34ff23c065c05bee0b
TakehiroKoretsune <oyayubi.cocacola@gmail.com>
2014-05-20 17:51:45 +0900

    pull test2

--- test/iOSViewController.h
+++ test/iOSViewController.h
@@ -5,10 +5,11 @@
 //  Created by Takehiro Koretsune on 2014/05/20.
 //  Copyright (c) 2014年 是常 雄大. All rights reserved.
 //
-testfgf
 
 #import <UIKit/UIKit.h>
 
 @interface iOSViewController : UIViewController
+
+test
 
 @end
