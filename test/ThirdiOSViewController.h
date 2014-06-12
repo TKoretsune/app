6ebf5f2279d6dcfca108c5fe0196bee7ba23188c
gucci-megane <gucci.magane.tks@gmail.com>
2014-06-13 05:04:01 +0900

    3つめの画面の追加、coredataのモデルを初期ファイルを作成

---
+++ test/ThirdiOSViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThirdiOSViewController.h
+//  test
+//
+//  Created by gucci on 2014/06/13.
+//  Copyright (c) 2014年 是常 雄大. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdiOSViewController : UIViewController
+
+@end
